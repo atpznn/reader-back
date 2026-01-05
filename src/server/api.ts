683bd06bc5a20d80ab808564f834c2010bdbdf80
atip.n@10xgroupholding.com <atip.n@10xgroupholding.com>
2026-01-05 13:37:49 +0700

    add logger

--- src/server/api.ts
+++ src/server/api.ts
@@ -5,6 +5,11 @@ const app: Express = express();
 const port: number = 8080;
 app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+  const requestTime = new Date(Date.now()).toISOString();
+  console.log(`[${requestTime}] ${req.method} ${req.url}`);
+  next();
+});
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, Express with TypeScript!");
 });
