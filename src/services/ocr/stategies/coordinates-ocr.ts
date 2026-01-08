a86b22a57adbb512d981a30bff3298d2d084ac31
atip.n@10xgroupholding.com <atip.n@10xgroupholding.com>
2026-01-08 15:16:34 +0700

    update config

--- src/services/ocr/stategies/coordinates-ocr.ts
+++ src/services/ocr/stategies/coordinates-ocr.ts
@@ -10,8 +10,8 @@ export class CoordinatesOcrStategy implements OcrStategy {
 
         "textord_heavy_nr": "0",
         "textord_min_linesize": '1.0',
-        "tessedit_do_invest": "0",
-        "paragraph_text_based": "0",
+        // "tessedit_do_invest": "0",
+        // "paragraph_text_based": "0",
         "tessdata_manager_debug_level": "0"
     }
     public getConfig(): tesseract.Config {
